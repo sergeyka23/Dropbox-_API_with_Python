8d793960d9aa46efd0a53f08a0afd701a7f52df3
sergeyka23 <sergeyd23@gmail.com>
2022-12-27 22:16:52 +0200

    working project- authentication is missing

--- OauthFlow.py
+++ OauthFlow.py
@@ -3,12 +3,13 @@
 import dropbox
 from dropbox import DropboxOAuth2FlowNoRedirect
 
-
+# This code will generate a manual long-lasting token
 APP_KEY = ""
 APP_SECRET = ""
 
 auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
 
+# Generates a link to creating authorization token
 authorize_url = auth_flow.start()
 print("1. Go to: " + authorize_url)
 print("2. Click \"Allow\" (you might have to log in first).")
@@ -16,10 +17,10 @@ print("3. Copy the authorization code.")
 auth_code = input("Enter the authorization code here: ").strip()
 
 try:
-    access_token = auth_flow.finish(auth_code)
-    #access_token, _ = web_auth.finish(code)
+    oauth_result = auth_flow.finish(auth_code)
 
-    print("Access token:", access_token)
+# Printed token will be copied manually to use in future application
+    print("Access token:", oauth_result)
 except Exception as e:
     print('Error: %s' % (e,))
     exit(1)
